b709dae133851d979ba438355b287e19ac83ccff
David Verghese <dj.verghese@gmail.com>
2025-04-27 22:36:18 -0400

    Embed a random monkey video

---
+++ app.js
@@ -0,0 +1,61 @@
+let apiKey;
+
+function getRandomIndex(videoListLength) {
+  return Math.floor(Math.random() * videoListLength)
+}
+
+function appendVideo(videoId) {
+  const iframe = document.createElement('iframe');
+  iframe.src = `https://www.youtube.com/embed/${videoId}`;
+  iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+  iframe.allowFullscreen = true;
+  const container = document.getElementById('video-container');
+  container.appendChild(iframe);
+}
+
+async function getApiKey() {
+  const response = await fetch('http://localhost:3000/apikey');
+  const data = await response.json();
+  return data.apiKey;
+}
+
+async function getRandomMonkeyVideo(searchQuery, maxResults, apiKey) {
+  try {
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${encodeURIComponent(searchQuery)}&type=video&key=${apiKey}`);
+    const data = await response.json();
+    
+    const backupVideoList = [
+      'b0NHrFNZWh0',
+      'vOY6x4NksIk',
+      'kUG9JdigH54'
+    ]
+
+    if (response.ok) {
+      if (data.items && data.items.length > 0) {
+        const randomIndex = getRandomIndex(data.items.length);
+        const videoId = data.items[randomIndex].id.videoId;
+        appendVideo(videoId);
+      } else {
+        console.error('No videos found.');
+      }
+    } else {
+      const randomIndex = getRandomIndex(backupVideoList.length)
+      const videoId = backupVideoList[randomIndex];
+      appendVideo(videoId)
+    }
+  } catch (error) {
+    console.error('Error fetching video:', error);
+  }
+}
+
+async function fetchMonkeyVideo() {
+  apiKey = await getApiKey();
+
+  const searchQuery = 'monkey';
+  const maxResults = 10; // number of results to pull
+
+  getRandomMonkeyVideo(searchQuery, maxResults, apiKey);
+
+}
+
+fetchMonkeyVideo();
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(cors());
 
 app.get('/apikey', (req, res) => {
   res.json({ apiKey: process.env.API_KEY });
